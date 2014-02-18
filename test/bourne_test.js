d3a138596b776d2c556913d40fd0d310d7615869
Andrew <andrew8088@gmail.com>
2014-02-18 16:00:22 -0500

    added constructor function

--- test/bourne_test.js
+++ test/bourne_test.js
@@ -2,17 +2,27 @@
 
 if (typeof require !== 'undefined') {
     var Bourne = require('../lib/bourne.js');
+    var Chance = require('chance');
 }
+
+var c = new Chance();
+
+var testName = (typeof __dirname !== 'undefined') ? __dirname + '/' : '';
+testName += c.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+
+var testRecord1 = { firstname: c.first(), lastname: c.last(), age: c.age(), birthday: c.birthday() };
 
 this.bourne_test = {
   setUp: function(done) {
-    // setup here
     done();
   },
-  'no args': function(test) {
+  'can create Bourne instance': function(test) {
     test.expect(1);
-    // tests here
-    test.equal(Bourne.awesome(), 'awesome', 'should be awesome.');
+
+    test.doesNotThrow(function () {
+        var db = new Bourne(testName, { reset: true });
+    });
+
     test.done();
-  },
+  }
 };
