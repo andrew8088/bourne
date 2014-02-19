4f02b0bf2f2e52e434a845450945b2f3a95b91f6
Andrew <andrew8088@gmail.com>
2014-02-19 13:00:51 -0500

    added initial find implementation

--- test/bourne_test.js
+++ test/bourne_test.js
@@ -10,11 +10,18 @@ var c = new Chance();
 var testName = (typeof __dirname !== 'undefined') ? __dirname + '/' : '';
 testName += c.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
 
-var testRecord1 = { firstname: c.first(), lastname: c.last(), age: c.age(), birthday: c.birthday() };
+var testRecord1 = { firstname: c.first(), lastname: c.last(), age: c.age(), birthday: c.birthday() },
+    testRecord2 = { firstname: c.first(), lastname: c.last(), age: c.age(), birthday: c.birthday() },
+    testRecord3 = { firstname: c.first(), lastname: c.last(), age: c.age(), birthday: c.birthday() };
 
 this.bourne_test = {
     setUp: function(done) {
-        done();
+        var db = this.db = new Bourne(testName, { reset: true });
+        db.insert(testRecord1, function () {
+            db.insert(testRecord2, function () {
+                db.insert(testRecord3, done);
+            });
+        });
     },
     'can create Bourne instance': function(test) {
         test.expect(1);
@@ -41,6 +48,31 @@ this.bourne_test = {
             test.equal(db2.data[0].firstname, testRecord1.firstname, 'names should be equal');
             test.done();
         });
-        
+    },
+    'can find records by one key': function (test) {
+        this.db.find({ firstname: testRecord1.firstname }, function (err, records) {
+            test.equal(records.length, 1, 'should find one record');
+            test.equal(records[0].firstname, testRecord1.firstname, 'names should be equal');
+            test.done();
+        });
+    },
+    'can find records by multiple keys': function (test) {
+        this.db.find({ firstname: testRecord1.firstname, age: testRecord1.age }, function (err, records) {
+            test.equal(records.length, 1, 'should find one record');
+            test.equal(records[0].age, testRecord1.age, 'names should be equal');
+            test.done();
+        });
+    },
+    'can find multiple records': function (test) {
+        var r = { firstname: testRecord1.firstname, age: c.age() };
+
+        var db = this.db;
+
+        db.insert(r, function () {
+            db.find({ firstname: testRecord1.firstname }, function (err, records) {
+                test.equal(records.length, 2, '2 records should be found');
+                test.done();
+            });
+        });
     }
 };
